8a7148aefa78215a07e5c1f386a791ce862ca7b1
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-03-24 13:12:52 -0500

    styling & percentage functions

--- public/JS/calc_js.js
+++ public/JS/calc_js.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var firstValue = document.getElementById('first_value');
-var operand = document.getElementById('operand');
+var operator = document.getElementById('operator');
 var secondValue = document.getElementById('second_value');
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
@@ -21,19 +21,19 @@ var btnDiv = document.getElementById('btnDiv');
 var btnMulti = document.getElementById('btnMulti');
 var btnSol = document.getElementById('btnSol');
 var btnDec = document.getElementById('btnDec');
+var btnPer = document.getElementById('btnPer');
 
 var btnDecListener = function() {
-    if (!operand.value) {
+    if (!operator.value) {
         firstValue.value += ".";
     } else {
         secondValue.value += ".";
     }
 }
-
 btnDec.addEventListener('click', btnDecListener);
 
 var btn1Listener = function() {
-    if (!operand.value) {
+    if (!operator.value) {
         firstValue.value += 1;
     } else {
         secondValue.value += 1;
@@ -43,7 +43,7 @@ var btn1Listener = function() {
 btn1.addEventListener('click', btn1Listener);
 
 var btn2Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 2;
     } else {
         secondValue.value += 2;
@@ -52,7 +52,7 @@ var btn2Listener = function() {
 btn2.addEventListener('click', btn2Listener);
 
 var btn3Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 3;
     } else {
         secondValue.value += 3;
@@ -61,7 +61,7 @@ var btn3Listener = function() {
 btn3.addEventListener('click', btn3Listener);
 
 var btn4Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 4;
     } else {
         secondValue.value += 4;
@@ -70,7 +70,7 @@ var btn4Listener = function() {
 btn4.addEventListener('click', btn4Listener);
 
 var btn5Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 5;
     } else {
         secondValue.value += 5;
@@ -79,7 +79,7 @@ var btn5Listener = function() {
 btn5.addEventListener('click', btn5Listener);
 
 var btn6Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 6;
     } else {
         secondValue.value += 6;
@@ -88,7 +88,7 @@ var btn6Listener = function() {
 btn6.addEventListener('click', btn6Listener);
 
 var btn7Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 7;
     } else {
         secondValue.value += 7;
@@ -97,7 +97,7 @@ var btn7Listener = function() {
 btn7.addEventListener('click', btn7Listener);
 
 var btn8Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 8;
     } else {
         secondValue.value += 8;
@@ -106,7 +106,7 @@ var btn8Listener = function() {
 btn8.addEventListener('click', btn8Listener);
 
 var btn9Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 9;
     } else {
         secondValue.value += 9;
@@ -115,7 +115,7 @@ var btn9Listener = function() {
 btn9.addEventListener('click', btn9Listener);
 
 var btn0Listener = function() {
-        if (!operand.value) {
+        if (!operator.value) {
         firstValue.value += 0;
     } else {
         secondValue.value += 0;
@@ -125,15 +125,28 @@ btn0.addEventListener('click', btn0Listener);
 //Clear
 
 var btnCListener = function() {
-    firstValue.value = '';
-    secondValue.value = '';
-    operand.value = '';
+    switch (firstValue.value.length > 0 || secondValue.value.length > 0 || operator.value.length > 0) {
+        case (secondValue.value.length > 0):
+            secondValue.value = secondValue.value.substring(0, secondValue.value.length - 1);
+            break;
+        case (operator.value.length > 0):
+            operator.value = operator.value.substring(0, operator.value.length - 1);
+            break;
+        case (firstValue.value.length > 0):
+            firstValue.value = firstValue.value.substring(0, firstValue.value.length - 1);
+            break;
+    }
 }
 btnC.addEventListener('click', btnCListener);
 //Subtraction
 
 var btnMinusListener = function() {
-    operand.value = "-";
+    if (!firstValue.value) {
+        firstValue.value += "-";
+    } else if (operator.value) {
+        secondValue.value = "-";
+    } else operator.value = "-";
+
 }
 
 
@@ -141,29 +154,34 @@ btnMinus.addEventListener('click', btnMinusListener);
 //Addition
 
 var btnAddListener = function() {
-    operand.value = "+";
+    operator.value = "+";
     
 }
 btnAdd.addEventListener('click', btnAddListener);
 //Division
 
 var btnDivListener = function() {
-    operand.value = "/";
+    operator.value = "/";
 }
 btnDiv.addEventListener('click', btnDivListener);
 //Multiplication
 
-var btnMultiListener = function() {
-    operand.value = "*";
-
+var btnMultiListener = function() { 
+        operator.value = "*";
 }
 btnMulti.addEventListener('click', btnMultiListener);
+//Percentage
+
+var btnPerListener = function() {
+    operator.value = "%";
+}
+btnPer.addEventListener('click', btnPerListener)
 //Solution
 
 var btnSolListener = function() {
 
 
-    switch (operand.value) {
+    switch (operator.value) {
         case "-":
             firstValue.value = firstValue.value - secondValue.value;
             break;
@@ -175,11 +193,24 @@ var btnSolListener = function() {
             break;
         case "*":
             firstValue.value = firstValue.value * secondValue.value;
+            break;
+        case "%":
+            firstValue.value = (firstValue.value / 100) * secondValue.value;
+            break;
     }
-    operand.value = "";
+    
+
+    if (firstValue.value == "NaN") {
+            operator.value = "";
+            secondValue.value = "ERROR";
+            firstValue.value = "ERROR"
+            setTimeout(function() {
+            firstValue.value = "";
+        }, 1000)
+    }
     secondValue.value = "";
 }   
 btnSol.addEventListener('click', btnSolListener);
 
-
 })();
+
