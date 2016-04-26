f927ec3fead231018fc6a5a947d8964947dfce6d
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-26 11:12:14 -0500

    mvc exercise

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,24 +1,29 @@
 <?php
 
-$adjectives = ["Spotty", "Whispering", "Lazy", "Cuddly", "Relieved", "Jittery", "Confused", "Grumpy", "Shaggy", "Elated"];
-$nouns = ["Unicorn", "Chair", "Cat", "Toaster", "Sandwich", "Frog", "Apple", "Princess", "Train", "Fish"];
-$colors = ["AliceBlue", "Charteuse", "CadetBlue", "Crimson", "Cyan", "Darkgreen", "DeepSkyBlue", "Gold", "Indigo", "Tomato"];
-$randomColor = mt_rand(0, 9);
 
-function random($array1, $array2) {
-$length1 = count($array1) - 1;
-$length2 = count($array2) - 1;
-$firstRand = mt_rand(0, $length1);
-$secondRand = mt_rand(0, $length2);
-return "{$array1[$firstRand]} {$array2[$secondRand]}\n";
+function pageController() {
+    $adjectives = ["Spotty", "Whispering", "Lazy", "Cuddly", "Relieved", "Jittery", "Confused", "Grumpy", "Shaggy", "Elated"];
+    $nouns = ["Unicorn", "Chair", "Cat", "Toaster", "Sandwich", "Frog", "Apple", "Princess", "Train", "Fish"];
+    $colors = ["AliceBlue", "Charteuse", "CadetBlue", "Crimson", "Cyan", "Darkgreen", "DeepSkyBlue", "Gold", "Indigo", "Tomato"];
+    $randomColor = mt_rand(0, 9);
+    $length1 = count($adjectives) - 1;
+    $length2 = count($nouns) - 1;
+    $firstRand = mt_rand(0, $length1);
+    $secondRand = mt_rand(0, $length2);
+    return [
+        "randomName" => "{$adjectives[$firstRand]} {$nouns[$secondRand]}\n",
+        "randomColor" => "{$colors[$randomColor]}"
+    ];
 }
+
+extract(pageController());
 ?>
 <!DOCTYPE html>
 <html> 
 <head>
 <style>
     body {
-        background-color: <?= $colors[$randomColor] ?>;
+        background-color: <?= $randomColor ?>;
     }
     h1 {
         margin-top: 150px;
@@ -29,6 +34,6 @@ return "{$array1[$firstRand]} {$array2[$secondRand]}\n";
 </style>
 </head>
 <body>
-<h1><?= random($adjectives, $nouns); ?></h1>
+<h1><?= $randomName ?></h1>
 
 </body>
