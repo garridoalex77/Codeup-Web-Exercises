26140363a705a873f5541fcb5ba9b6d09c6ea15f
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-12 09:55:09 -0500

    update

--- pdo_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'employees');
-define('DB_USER', 'codeup');
-define('DB_PASS', 'Jayla2007!');
-
-require 'db_connect.php';
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
