69950803885a93f9861e4b8e3e18a63d2ed91497
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-12 10:13:58 -0500

    update for pw

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -4,25 +4,5 @@ function pageController() {
     require_once '../Generate.php';
     return ["randomColor" => Generate::getColor(), "randomName" => Generate::generatingNames()];
 }
-
 extract(pageController());
-?>
-<!DOCTYPE html>
-<html> 
-<head>
-<style>
-    body {
-        background-color: <?= $randomColor ?>;
-    }
-    h1 {
-        margin-top: 150px;
-        text-align: center;
-        font-size: 3em;
-        color: orchid;
-    }
-</style>
-</head>
-<body>
-<h1><?= $randomName ?></h1>
-
-</body>
+echo $randomName.PHP_EOL;
