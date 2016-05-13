497b5dab03ecffc17e2da09cdc68014be3cf0b2f
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-05-13 13:15:34 -0500

    working parks page

--- public/national_parks.php
+++ public/national_parks.php
@@ -3,22 +3,30 @@ require_once '../parks_db_cred.php';
 require_once '../db_connect.php';
 function pageController($dbc) {
     $data = [];
-    $stmt = $dbc->query('SELECT * FROM national_parks');
+    if (isset($_GET['offset']) && $_GET['offset'] < 0) {
+        $data['offset'] = 0;
+    } elseif (isset($_GET['offset'])) {
+        $data['offset'] = $_GET['offset'];
+    } else {
+        $data['offset'] = 0;
+    }
+    $stmt = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET {$data['offset']}");
     $data['parks'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     return $data;
 }
 extract(pageController($dbc));
 ?>
-
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>National Parks</title>
 </head>
 <body>
+<form method="GET" action="/national_parks.php">
+    <button type="submit" name="offset" value=<?= $offset - 4 ?>>Get Previous Park</button>
+    <button type="submit" name="offset" value=<?= $offset + 4 ?>>Get Next Park</button>
+</form>
     <h1>National Parks</h1>
     <table>
         <tr>
@@ -30,7 +38,7 @@ extract(pageController($dbc));
         </tr>
         <?php foreach ($parks as $park) { ?>
         <tr>
-            <?php foreach ($park as $title => $value) { ?>
+            <?php foreach ($park as $value) { ?>
             <td><?= $value ?></td>
             <?php };?>
         </tr>
